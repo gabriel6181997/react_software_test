20fe0d817320b7adae56b568b5a74ac4082701fb
gabriel6181997 <gabriel6181997@gmail.com>
2022-10-27 23:11:25 +0900

    ✅ Add tests for render component

---
+++ src/Render.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable testing-library/no-debugging-utils */
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Render from "./Render";
+
+describe("Rendering", () => {
+  it("Should render all the elements correctly", () => {
+    render(<Render />);
+    // screen.debug();
+    // screen.debug(screen.getByRole("heading"));
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    expect(screen.getByText("Udemy")).toBeTruthy();
+    expect(screen.queryByText("Udeeemy")).toBeNull();
+    expect(screen.getByTestId("copyright")).toBeTruthy();
+    // screen.debug(screen.getByText("Udemy"));
+  });
+});
